bb8d09ac52a3e188c1eb161b4156794824f4c82a
Annick FROMAGEAT <annick.fromageat@gmail.com>
2022-10-11 11:31:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php' ;
+// autoload.php qui se trouve dans ton dossier vendor
+
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+// $output = $bessie->say();
+// echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
